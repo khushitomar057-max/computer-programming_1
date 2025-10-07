0afc4ec87ff31ee38ba87a6470f38cd17b2a1d6d
khushitomar <khushitomar057@gmail.com>
2025-10-07 09:47:04 +0530

    Create skipping values in array.c

---
+++ skipping values in array.c
@@ -0,0 +1,24 @@
+
+#include<stdio.h>
+int main(){
+   int arr1[5],arr2[10];
+   for(i=0;i<5;i++){
+      scanf("%d",&arr[i]);
+   }
+   for(i=0;i<5;i++){
+      arr2[j]=arr1[i];
+      j+=2;
+   }
+   for(i=0;i<10;i++){
+      if(arr2[i]==0 && i%2!=0){
+         arr[i]=0;
+      }
+   }
+   printf("new array will be");
+   for(i=0;i<0;i++){
+      printf("%d",arr2[i]);
+
+   }
+   return 0;
+}
+   
