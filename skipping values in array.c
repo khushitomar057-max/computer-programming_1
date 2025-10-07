fa34c0bd7eae82329a0d2b82a6674096868a0c9c
khushitomar <khushitomar057@gmail.com>
2025-10-07 09:59:26 +0530

    Update skipping values in array.c

--- skipping values in array.c
+++ skipping values in array.c
@@ -2,8 +2,9 @@
 #include<stdio.h>
 int main(){
    int arr1[5],arr2[10];
+   int i,j;
    for(i=0;i<5;i++){
-      scanf("%d",&arr[i]);
+      scanf("%d",&arr1[i]);
    }
    for(i=0;i<5;i++){
       arr2[j]=arr1[i];
@@ -11,14 +12,15 @@ int main(){
    }
    for(i=0;i<10;i++){
       if(arr2[i]==0 && i%2!=0){
-         arr[i]=0;
+         arr2[i]=0;
       }
    }
    printf("new array will be");
-   for(i=0;i<0;i++){
+   for(i=0;i<10;i++){
       printf("%d",arr2[i]);
 
    }
    return 0;
 }
+   
    
