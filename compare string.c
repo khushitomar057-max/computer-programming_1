357a7ee114d11106a419eae809186ef5c8091d05
khushitomar <khushitomar057@gmail.com>
2025-11-20 22:09:28 +0530

    Create compare string.c

---
+++ compare string.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+char str1[100];
+char str2[100];
+int i,flag=0;
+printf("enter string 1");
+gets(str1);
+printf("enter string 2");
+gets(str2);
+
+for(i=0;str1[i]!='\0'|| str2[i]!='\0';i++){
+    if(str1[i]!=str2[i]){
+        flag=1;
+        break;
+    }
+}
+if(flag==0)
+printf("string are equal");
+else
+printf("not equal");
+
+
+return 0;  
+}
